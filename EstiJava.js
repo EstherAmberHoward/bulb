bdd8fb6f1300257166f9879909d7844108e83559
EstherAmberHoward <ms.esther.howard@gmail.com>
2025-05-04 15:47:03 +0200

    Update EstiJava.js
    added PORTFOLIO TABS

--- EstiJava.js
+++ EstiJava.js
@@ -11,3 +11,16 @@ function onHamburgerClick() {
         navbar.classList.remove("open");
     }
 }
+function openSection(evt, sectionName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(sectionName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
