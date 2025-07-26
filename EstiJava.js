1af52978e4f2ac8825fd41b332a70b5281bf4fbc
EstherAmberHoward <ms.esther.howard@gmail.com>
2025-07-26 15:56:26 +0200

    Update EstiJava.js
    trying to add whole page overlay

--- EstiJava.js
+++ EstiJava.js
@@ -16,10 +16,12 @@ function openSection (evt, sectionName) {
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+  document.getElementById(sectionName).style.height = "0%";
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(sectionName).style.display = "block"; evt.currentTarget.className += " active";
+  document.getElementById(sectionName).style.height = "100%";
 }
